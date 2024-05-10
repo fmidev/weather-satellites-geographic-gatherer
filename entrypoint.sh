5c5e19c3cf7634c792148b62cdbfdcab71fa4f1e
Panu Lahtinen <panu.lahtinen@fmi.fi>
2024-05-10 14:09:47 +0300

    Fix command-line argument passing

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ source /opt/conda/.bashrc
 source /config/env-variables
 
 micromamba activate
-/opt/conda/bin/geographic_gatherer.py -n false -i ${ADDRESSES} -c /config/geographic_gatherer.ini
+/opt/conda/bin/geographic_gatherer.py -n false -i ${ADDRESSES} /config/geographic_gatherer.ini
