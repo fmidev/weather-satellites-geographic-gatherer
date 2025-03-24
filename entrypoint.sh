f58012bdbeca150adfe2562a3d62d781aad63903
Panu Lahtinen <panu.lahtinen@fmi.fi>
2025-03-24 10:54:19 +0200

    Add SIGTERM handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/bash
 
+_term() {
+  echo "Entrypoint caught SIGTERM signal"
+  kill -TERM "$child" 2>/dev/null
+  echo "Waiting for child process to exit"
+  wait "$child"
+}
+
+trap _term SIGTERM
+
 source /opt/conda/.bashrc
 source /config/env-variables
 
 micromamba activate
-/opt/conda/bin/geographic_gatherer.py -p ${PUBLISH_PORT} -n false -i ${ADDRESSES} /config/geographic_gatherer.ini
+/opt/conda/bin/geographic_gatherer.py -p ${PUBLISH_PORT} -n false -i ${ADDRESSES} /config/geographic_gatherer.ini &
+
+child=$!
+
+wait "$child"
